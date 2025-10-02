8787b4e69834675f1ee8c8152063952d4f382117
Guzz <bragadagugudan@gmail.com>
2025-10-02 12:40:48 -0300

    Set up validator.js skeleton

---
+++ src/modules/validator.js
@@ -0,0 +1,21 @@
+export default class Validator {
+    validateEmail(string) {
+
+    }
+
+    validateCountry(string) {
+
+    }
+
+    validatePostalCode(string) {
+
+    }
+
+    validatePassword(string) {
+
+    }
+
+    confirmPassword(pswd, string) {
+        
+    }
+}
